2179764d6d8a9891a8a82de37a5e11c7d2d4e391
Roman Stetsiuk <romansteciuk021@gmail.com>
2019-07-30 19:08:27 +0200

    Add image to MySQL

--- src/main/java/jdbc/Main.java
+++ src/main/java/jdbc/Main.java
@@ -1,10 +1,15 @@
 package jdbc;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.sql.*;
 
 public class Main {
 
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException {
 
         //        connect with DB MySQL
         String userName = "root";
@@ -17,7 +22,7 @@ public class Main {
              Statement statement = connection.createStatement()) {
 
             statement.executeUpdate("CREATE TABLE IF NOT EXISTS users(user_id INT NOT NULL AUTO_INCREMENT," +
-                    "name VARCHAR(30), information VARCHAR(30), PRIMARY KEY (user_id))");
+                    "name VARCHAR(30), information VARCHAR(30), image BLOB, PRIMARY KEY (user_id))");
 
             statement.executeUpdate("INSERT INTO users (name, information) VALUES ('user 1', 'new user')");
             statement.executeUpdate("INSERT INTO users SET name='user 2', information='lorem ipsum'");
@@ -32,6 +37,18 @@ public class Main {
                 System.out.println("user name: " + resultSet.getString("name"));
                 System.out.println("user information: " + resultSet.getString("information"));
             }
+
+//            Add image to DataBase
+            BufferedImage image = ImageIO.read(new File("icon.jpg"));
+            Blob blob = connection.createBlob();
+            try (OutputStream outputStream = blob.setBinaryStream(1)) {
+                ImageIO.write(image, "jpg", outputStream);
+            }
+            PreparedStatement statement1 = connection.prepareStatement
+                    ("INSERT INTO users (name, image) VALUES (?,?)");
+            statement1.setString(1, "user 1");
+            statement1.setBlob(2, blob);
+            statement1.execute();
 
         }
 
